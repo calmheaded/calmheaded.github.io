cfc38be29d3aef968b1e9c6d0044c2b3c22d2396
Admin <960301328@qq.com>
2023-01-05 17:14:21 +0800

    first

---
+++ libs/loaders/occt-import-js-worker.js
@@ -0,0 +1,13 @@
+importScripts ('occt-import-js.js');
+
+onmessage = async function (ev)
+{
+	let occt = await occtimportjs ();
+	let result = null;
+	if (ev.data.format === 'step') {
+		result = occt.ReadStepFile (ev.data.buffer);
+	} else if (ev.data.format === 'iges') {
+		result = occt.ReadIgesFile (ev.data.buffer);
+	}
+	postMessage (result);
+};
